6b4abea193e97658cc08a99513b25a3a3e3edb20
IgnjatR <ignjatrajak@gmail.com>
2019-06-26 21:40:03 +0200

    Implementirane metode sacuvajUJSONFajl i ucitajKnjigeIzJOSN

--- src/biblioteka/Biblioteka.java
+++ src/biblioteka/Biblioteka.java
@@ -1,22 +1,31 @@
 package biblioteka;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
 
 import biblioteka.interfejs.BibliotekaInterface;
 
 public class Biblioteka implements BibliotekaInterface {
-	
+
+	// Gson gson = new Gson();
 	private List<Knjiga> knjige = new LinkedList<Knjiga>();
 
 	@Override
 	public void dodajKnjigu(Knjiga k) {
 		if (k == null)
 			throw new NullPointerException("Knjiga ne sme biti null");
-		
+
 		if (knjige.contains(k))
 			throw new RuntimeException("Knjiga ne sme vec postojati u biblioteci");
-		
+
 		knjige.add(k);
 	}
 
@@ -37,26 +46,42 @@ public class Biblioteka implements BibliotekaInterface {
 	public List<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
 		if (autor == null && isbn == null && naslov == null && izdavac == null)
 			throw new RuntimeException("Morate uneti bar jedan kriterijum za pretragu");
-		
+
 		LinkedList<Knjiga> rezultati = new LinkedList<Knjiga>();
-		
-		for (Knjiga knjiga: knjige)
+
+		for (Knjiga knjiga : knjige)
 			if (knjiga.getNaslov().contains(naslov))
 				rezultati.add(knjiga);
-		
+
 		return rezultati;
 	}
 
 	@Override
 	public void sacuvajUJSONFajl(String putanja) {
-		// TODO Auto-generated method stub
-		
+		try (FileWriter out = new FileWriter(putanja)) {
+
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+			out.write(gson.toJson(knjige.toArray()));
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
 	public void ucitajKnjigeIzJSON(String putanja) {
-		// TODO Auto-generated method stub
-		
+
+		try (FileReader in = new FileReader(putanja)) {
+			knjige.removeAll(knjige);
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+			knjige = Arrays.asList(gson.fromJson(in, Knjiga[].class));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
